3c0e42e78eb7c56a3c6f90921f18b8b75aaf2c19
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-10 23:18:40 +0100

    Migrate to version 3.8 (grafted from 846098142b8d30245eb9598aabf127d67e99f37d)

--- tests/test_electronic_mail_wizard.py
+++ tests/test_electronic_mail_wizard.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part electronic_mail_wizard module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ElectronicMailWizardTestCase(unittest.TestCase):
+class ElectronicMailWizardTestCase(ModuleTestCase):
     'Test Electronic Mail Wizard module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('electronic_mail_wizard')
-
-    def test0005views(self):
-        'Test views'
-        test_view('electronic_mail_wizard')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'electronic_mail_wizard'
 
 
 def suite():
