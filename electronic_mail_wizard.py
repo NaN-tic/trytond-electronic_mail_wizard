5c02321fdb68e9f7ad8139bdeebf74d0f9a9e7c7
Raimon Esteve <raimonesteve@gmail.com>
2026-07-20 10:36:06 +0200

    Replace to import trytond.config as config

--- electronic_mail_wizard.py
+++ electronic_mail_wizard.py
@@ -1,7 +1,7 @@
 # This file is part electronic_mail_wizard module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.config import config
+import trytond.config as config
 from trytond.model import ModelView, fields
 from trytond.pool import Pool
 from trytond.pyson import Eval
